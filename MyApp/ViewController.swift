833210cb4a20d4f225e62101cb6119ffba709730
Cliff <klifiam@gmail.com>
2021-12-09 11:52:02 +0100

    Initial commit

--- MyApp/ViewController.swift
+++ MyApp/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var btn2: UIButton!
+    @IBOutlet weak var lblMyApp: UILabel!
+    @IBOutlet weak var imgTestApp: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
 
-
+    @IBAction func btn1(_ sender: Any) {
+        if(self.imgTestApp.image == UIImage (named: "testImage")){
+            self.lblMyApp.text = "A BMW Classic"
+            self.imgTestApp.image = UIImage (named: "testImage2")
+        }else if(self.imgTestApp.image == UIImage (named: "testImage2")){
+            self.lblMyApp.text = "A Mustang Classic"
+            self.imgTestApp.image = UIImage (named: "testImage3")
+        }else{
+            self.imgTestApp.image = UIImage (named: "testImage")
+            self.btn2.setTitle("Next", for: .normal)
+        }
+    }
 }
 
